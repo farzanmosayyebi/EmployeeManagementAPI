f35b6aca300b5d37cf0cbb4c96b03e6dc8b8e630
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-09 00:06:43 +0330

    Added address service

---
+++ src/EmployeeManagement.Common/Interfaces/IAddressService.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeManagement.Common.DTOs;
+
+namespace EmployeeManagement.Common.Interfaces;
+
+public interface IAddressService
+{
+    Task<int> CreateAddressAsync(AddressCreate addressCreate);
+    Task<AddressGet> GetAddressAsync(AddressGet addressGet);
+    Task<List<AddressGet>> GetAllAddressesAsync();
+    Task UpdateAddressAsync(AddressUpdate addressUpdate);
+    Task DeleteAddressAsync(AddressDelete addressDelete);
+}
