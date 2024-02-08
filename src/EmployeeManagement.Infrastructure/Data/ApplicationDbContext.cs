35f467c1ab5197d63041c7afd2b2d9ed4c7db6f5
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-08 21:32:27 +0330

    Infrastructure, DbContext, etc. - Initial commit

---
+++ src/EmployeeManagement.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.Common.Model;
+
+namespace EmployeeManagement.Infrastructure.Data;
+public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Job> Jobs { get; set; }
+    public DbSet<Team> Teams { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string DbPath = Path.Combine("Data", "EmployeeManagement.db");
+        optionsBuilder.UseSqlite($"Filename={DbPath}");
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Employee>().HasKey(e => e.Id);
+        builder.Entity<Address>().HasKey(e => e.Id);
+        builder.Entity<Job>().HasKey(e => e.Id);
+        builder.Entity<Team>().HasKey(e => e.Id);
+
+        builder.Entity<Employee>().HasOne(e => e.Address);
+        builder.Entity<Employee>().HasOne(e => e.Job);
+
+        builder.Entity<Employee>().HasMany(e => e.Teams).WithMany(t => t.Employees);
+
+        base.OnModelCreating(builder);
+    }
+}
---
+++ src/EmployeeManagement.Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,95 @@
+﻿using EmployeeManagement.Common.Interfaces;
+using EmployeeManagement.Common.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EmployeeManagement.Infrastructure.Data;
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    private readonly ApplicationDbContext _context;
+    private DbSet<T> _dbSet;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public async Task<List<T>> GetFilteredAsync(Expression<Func<T, bool>>[] filters, int? skip, int? take, params Expression<Func<T, object>>[] includes)
+    {
+        IQueryable<T> query = _dbSet.AsQueryable<T>();
+
+        foreach(var filter in filters)
+        {
+            query = query.Where(filter);
+        }
+
+        foreach(var include in includes)
+        {
+            query = query.Include(include);
+        }
+
+        if (skip != null)
+            query = query.Skip(skip.Value);
+
+        if (take != null)
+            query = query.Take(take.Value);
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<List<T>> GetAsync(int? skip, int? take, params Expression<Func<T, object>>[] includes)
+    {
+        IQueryable<T> query = _dbSet.AsQueryable<T>();
+
+        foreach (var include in includes)
+            query = query.Include(include);
+
+        if (skip != null)
+            query = query.Skip(skip.Value);
+
+        if (take != null)
+            query = query.Take(take.Value);
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes)
+    {
+        IQueryable<T> query = _dbSet.AsQueryable<T>();
+
+        query = query.Where(e => e.Id == id);
+
+        foreach (var include in includes)
+            query = query.Include(include);
+
+        return await query.SingleOrDefaultAsync();
+    }
+
+    public async Task<int> InsertAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        return entity.Id;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public void Update(T entity)
+    {
+        if (_context.Entry(entity).State == EntityState.Detached)
+            _dbSet.Attach(entity);
+
+        _dbSet.Entry(entity).State = EntityState.Modified;
+        //_context.SaveChanges();
+    }
+    public void Delete(T entity)
+    {
+        if (_context.Entry(entity).State == EntityState.Detached)
+            _dbSet.Attach(entity);
+        
+        _dbSet.Remove(entity);
+    }
+}
