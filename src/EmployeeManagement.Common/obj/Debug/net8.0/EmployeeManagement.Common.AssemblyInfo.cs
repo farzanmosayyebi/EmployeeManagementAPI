ab19a93ef864c8af928929595e4424e47f9dfc87
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-09 00:23:42 +0330

    ...

--- src/EmployeeManagement.Common/obj/Debug/net8.0/EmployeeManagement.Common.AssemblyInfo.cs
+++ src/EmployeeManagement.Common/obj/Debug/net8.0/EmployeeManagement.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14603f83dc8a36487e136b3e69823cc2d8a3bb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
