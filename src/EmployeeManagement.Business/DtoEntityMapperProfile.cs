373641c0f8f80da33da0b705fd09c51195521b49
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-09 13:01:10 +0330

    Added AutoMapper Profile

---
+++ src/EmployeeManagement.Business/DtoEntityMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EmployeeManagement.Common.Models;
+using EmployeeManagement.Common.DTOs;
+
+namespace EmployeeManagement.Business;
+
+public class DtoEntityMapperProfile : Profile
+{
+    public DtoEntityMapperProfile()
+    {
+        CreateMap<AddressCreate, Address>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
+        CreateMap<AddressUpdate, Address>();
+        CreateMap<Address, AddressGet>();
+    }
+}
