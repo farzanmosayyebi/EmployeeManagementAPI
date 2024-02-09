438a5f971be3b77176b198b7184d148cea3323db
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-09 13:02:18 +0330

    Dependency injection configuration for the Address mapper

---
+++ src/EmployeeManagement.Business/DependencyInjectionConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagement.Business.Services;
+using EmployeeManagement.Common.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagement.Business;
+
+public class DependencyInjectionConfiguration
+{
+    public static void RegisterServices(IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(DtoEntityMapperProfile));
+        services.AddScoped<IAddressService, AddressService>();
+    }
+}
