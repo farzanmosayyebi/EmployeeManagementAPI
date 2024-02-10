9b381d006bdb0e58607e9d54bb79411df163da54
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-10 19:27:16 +0330

    Added DTO mapper for Employee

--- src/EmployeeManagement.Business/DtoEntityMapperProfile.cs
+++ src/EmployeeManagement.Business/DtoEntityMapperProfile.cs
@@ -2,6 +2,7 @@
 using EmployeeManagement.Common.Models;
 using EmployeeManagement.Common.DTOs.Address;
 using EmployeeManagement.Common.DTOs.Job;
+using EmployeeManagement.Common.DTOs.Employee;
 
 namespace EmployeeManagement.Business;
 
@@ -18,6 +19,22 @@ public class DtoEntityMapperProfile : Profile
             .ForMember(j => j.Id, opt => opt.Ignore());
         CreateMap<JobUpdate, Job>();
         CreateMap<Job, JobGet>();
+
+        CreateMap<EmployeeCreate, Employee>()
+            .ForMember(e => e.Id, opt => opt.Ignore())
+            .ForMember(e => e.Job, opt => opt.Ignore())
+            .ForMember(e => e.Teams, opt => opt.Ignore());
+
+        CreateMap<EmployeeUpdate, Employee>()
+            .ForMember(e => e.Job, opt => opt.Ignore())
+            .ForMember(e => e.Teams, opt => opt.Ignore());
+
+        CreateMap<Employee, EmployeeDetails>() //todo: add teams
+            .ForMember(e => e.Id, opt => opt.Ignore())
+            .ForMember(e => e.Address, opt => opt.Ignore())
+            .ForMember(e => e.Job, opt => opt.Ignore());
+
+        CreateMap<Employee, EmployeeList>();    
 
     }
 }
