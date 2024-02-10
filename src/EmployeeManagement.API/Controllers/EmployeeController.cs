6fd506a15f1722d884fef859fb498516219dd16c
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-10 19:25:51 +0330

    Implemented API Controller for Employee

---
+++ src/EmployeeManagement.API/Controllers/EmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using EmployeeManagement.Common.DTOs.Employee;
+using EmployeeManagement.Common.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployeeController : ControllerBase
+{
+    private readonly IEmployeeService _employeeService;
+
+    public EmployeeController(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+    }
+
+    [HttpPost]
+    [Route("Create")]
+    public async Task<IActionResult> CreateEmployee(EmployeeCreate employeeCreate)
+    {
+        var id = await _employeeService.CreateEmployeeAsync(employeeCreate);
+        return Ok(id);
+    }
+
+    [HttpGet]
+    [Route("Get/{id}")]
+    public async Task<IActionResult> GetEmployee(int id)
+    {
+        EmployeeDetails employee = await _employeeService.GetEmployeeAsync(id);
+        return Ok(employee);
+    }
+
+    [HttpGet]
+    [Route("Get")]
+    public async Task<IActionResult> FilterEmployees([FromQuery]EmployeeFilter employeeFilter)
+    {
+        List<EmployeeList> employees = await _employeeService.FilterEmployeesAsync(employeeFilter);
+        return Ok(employees);
+    }
+
+    [HttpPut]
+    [Route("Update")]
+    public async Task<IActionResult> UpdateEmployee(EmployeeUpdate employeeUpdate)
+    {
+        await _employeeService.UpdateEmployeeAsync(employeeUpdate);
+        return Ok();
+    }
+
+    [HttpDelete]
+    [Route("Delete")]
+    public async Task<IActionResult> DeleteEmployee(EmployeeDelete employeeDelete)
+    {
+        await _employeeService.DeleteEmployeeAsync(employeeDelete);
+        return Ok();
+    }
+}
