7ba2985a835bea600129a1ad1adce0836d5c7bd6
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-10 10:18:25 +0330

    added Job Service interface

---
+++ src/EmployeeManagement.Common/Interfaces/IJobService.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeManagement.Common.DTOs.Job;
+
+namespace EmployeeManagement.Common.Interfaces;
+public interface IJobService
+{
+    Task<int> CreateJobAsync(JobCreate jobCreate);
+    Task<JobGet> GetJobAsync(int id);
+    Task<List<JobGet>> GetAllJobsAsync();
+    Task DeleteJobAsync(JobDelete jobDelete);
+    Task UpdateJobAsync(JobUpdate jobUpdate);
+}
