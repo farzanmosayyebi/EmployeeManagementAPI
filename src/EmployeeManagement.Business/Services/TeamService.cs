15b9dba4fd8cde28837da309ccaa30aa48654f7f
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-11 15:29:01 +0330

    Implemented Team Service

---
+++ src/EmployeeManagement.Business/Services/TeamService.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using EmployeeManagement.Common.DTOs.Employee;
+using EmployeeManagement.Common.DTOs.Team;
+using EmployeeManagement.Common.Interfaces;
+using EmployeeManagement.Common.Models;
+using System.Linq.Expressions;
+
+namespace EmployeeManagement.Business.Services;
+
+internal class TeamService : ITeamService
+{
+    private readonly IGenericRepository<Team> _teamRepository;
+    private readonly IGenericRepository<Employee> _employeeRepository;
+    private readonly IMapper _mapper;
+
+    public TeamService(IGenericRepository<Team> teamRepository, IGenericRepository<Employee> employeeRepository, IMapper mapper)
+    {
+        _teamRepository = teamRepository;
+        _employeeRepository = employeeRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<int> CreateTeamAsync(TeamCreate teamCreate)
+    {
+        Team team = _mapper.Map<Team>(teamCreate);
+        int id = await _teamRepository.InsertAsync(team);
+        await _teamRepository.SaveChangesAsync();
+        return id;
+    }
+
+    public async Task AddEmployeesAsync(int teamId, List<int> employeeIds)
+    {
+        Expression<Func<Employee, bool>> employeeFilter = employee => employeeIds.Contains(employee.Id);
+
+        Team team = await _teamRepository.GetByIdAsync(teamId, t => t.Employees);
+
+
+        List<Employee> employeeEntities = await _employeeRepository.GetFilteredAsync([employeeFilter], null, null);
+        //employeeIds.ForEach(async id => employeeEntities.Add(await _employeeRepository.GetByIdAsync(id)));
+
+        team.Employees.AddRange(employeeEntities);
+        _teamRepository.Update(team);
+        
+        await _teamRepository.SaveChangesAsync();
+    }
+
+    public async Task DeleteTeamAsync(TeamDelete teamDelete)
+    {
+        Team team = await _teamRepository.GetByIdAsync(teamDelete.Id);
+        _teamRepository.Delete(team);
+        await _teamRepository.SaveChangesAsync();
+    }
+
+    public async Task<List<TeamGet>> FilterTeamsAsync(TeamFilter teamFilter)
+    {
+        Expression<Func<Team, bool>> nameFilter = t => teamFilter.Name == null ? true
+            : t.Name.StartsWith(teamFilter.Name);
+        
+        Expression<Func<Team, bool>> employeeFilter = t => teamFilter.EmployeeId == null ? true 
+            : t.Employees.Any(e => e.Id == teamFilter.EmployeeId);
+
+        Expression<Func<Team, bool>>[] filters = [nameFilter, employeeFilter];
+
+        List<Team> teams = await _teamRepository.GetFilteredAsync(filters, teamFilter.Skip, teamFilter.Take);
+
+        return _mapper.Map<List<TeamGet>>(teams);
+    }
+
+    public async Task<TeamGet> GetTeamAsync(int Id)
+    {
+        Team team = await _teamRepository.GetByIdAsync(Id, t => t.Employees);
+        
+        return _mapper.Map<TeamGet>(team);
+    }
+
+    public async Task RemoveEmployeesAsync(int teamId, List<int> employeeIds)
+    {
+        Expression<Func<Employee, bool>> employeeFilter = employee => employeeIds.Contains(employee.Id);
+        Team team = await _teamRepository.GetByIdAsync(teamId, t => t.Employees);
+
+        List<Employee> employeeEntities = await _employeeRepository.GetFilteredAsync([employeeFilter], null, null);
+        employeeEntities.ForEach(e => team.Employees.Remove(e));
+
+        _teamRepository.Update(team);
+        await _teamRepository.SaveChangesAsync();
+    }
+
+    public async Task UpdateTeamAsync(TeamUpdate teamUpdate)
+    {
+        Team team = _mapper.Map<Team>(teamUpdate);
+
+        _teamRepository.Update(team);
+        await _teamRepository.SaveChangesAsync();
+    }
+}
---
+++ src/EmployeeManagement.Common/Interfaces/ITeamService.cs
@@ -0,0 +1,15 @@
+﻿using EmployeeManagement.Common.DTOs.Employee;
+using EmployeeManagement.Common.DTOs.Team;
+
+namespace EmployeeManagement.Common.Interfaces;
+
+public interface ITeamService
+{
+    Task<int> CreateTeamAsync(TeamCreate teamCreate);
+    Task<TeamGet> GetTeamAsync(int Id);
+    Task<List<TeamGet>> FilterTeamsAsync(TeamFilter teamFilter);
+    Task AddEmployeesAsync(int teamId, List<int> employeeIds);
+    Task RemoveEmployeesAsync(int teamId, List<int> employeeIds);
+    Task UpdateTeamAsync(TeamUpdate teamUpdate);
+    Task DeleteTeamAsync(TeamDelete teamDelete);
+}
