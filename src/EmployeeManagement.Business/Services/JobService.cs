6c1afcbd30bc12e94c573294326b748b8f1fd7f3
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-13 20:33:35 +0330

    Implemented error handling

--- src/EmployeeManagement.Business/Services/JobService.cs
+++ src/EmployeeManagement.Business/Services/JobService.cs
@@ -1,22 +1,26 @@
 ﻿using AutoMapper;
+using EmployeeManagement.Business.Exceptions;
 using EmployeeManagement.Business.Validation.Job;
 using EmployeeManagement.Common.DTOs.Job;
 using EmployeeManagement.Common.Interfaces;
 using EmployeeManagement.Common.Models;
 using FluentValidation;
+using System.Linq.Expressions;
 
 namespace EmployeeManagement.Business.Services
 {
     public class JobService : IJobService
     {
-        private readonly IGenericRepository<Job> _repository;
+        private readonly IGenericRepository<Job> _jobRepository;
+        private readonly IGenericRepository<Employee> _employeeRepository;
         private readonly IMapper _mapper;
         private readonly JobCreateValidator _createValidator;
         private readonly JobUpdateValidator _updateValidator;
 
-        public JobService(IGenericRepository<Job> genericRepository, IMapper mapper, JobCreateValidator createValidator, JobUpdateValidator updateValidator)
+        public JobService(IGenericRepository<Job> genericRepository, IGenericRepository<Employee> employeeRepository, IMapper mapper, JobCreateValidator createValidator, JobUpdateValidator updateValidator)
         {
-            _repository = genericRepository;
+            _jobRepository = genericRepository;
+            _employeeRepository = employeeRepository;
             _mapper = mapper;
             _createValidator = createValidator;
             _updateValidator = updateValidator;
@@ -27,29 +31,41 @@ namespace EmployeeManagement.Business.Services
 
             var entity = _mapper.Map<Job>(jobCreate);
 
-            await _repository.InsertAsync(entity);
-            await _repository.SaveChangesAsync();
+            await _jobRepository.InsertAsync(entity);
+            await _jobRepository.SaveChangesAsync();
 
             return entity.Id;
         }
 
         public async Task DeleteJobAsync(JobDelete jobDelete)
         {
-            var entity = await _repository.GetByIdAsync(jobDelete.Id);
-            _repository.Delete(entity);
-            await _repository.SaveChangesAsync();
+            var entity = await _jobRepository.GetByIdAsync(jobDelete.Id);
+
+            if (entity == null)
+                throw new JobNotFoundException(jobDelete.Id);
+
+            Expression<Func<Employee, bool>> employeesWithThisJob = employee => employee.Job.Id == jobDelete.Id;
+            List<Employee> dependentEmployees = await _employeeRepository.GetFilteredAsync([employeesWithThisJob], null, null);
+            if (dependentEmployees.Count > 0)
+                throw new DependentEmployeesExistException(dependentEmployees);
+
+            _jobRepository.Delete(entity);
+            await _jobRepository.SaveChangesAsync();
         }
 
         public async Task<List<JobGet>> GetAllJobsAsync()
         {
-            var entityList = await _repository.GetAsync(null, null);
+            var entityList = await _jobRepository.GetAsync(null, null);
 
             return _mapper.Map<List<JobGet>>(entityList);
         }
 
         public async Task<JobGet> GetJobAsync(int id)
         {
-            var entity = await _repository.GetByIdAsync(id);
+            var entity = await _jobRepository.GetByIdAsync(id);
+            
+            if (entity == null)
+                throw new JobNotFoundException(id);
 
             return _mapper.Map<JobGet>(entity);
         }
@@ -58,10 +74,14 @@ namespace EmployeeManagement.Business.Services
         {
             await _updateValidator.ValidateAndThrowAsync(jobUpdate);
 
+            var existingEntity = await _jobRepository.GetByIdAsync(jobUpdate.Id);
+            if (existingEntity == null)
+                throw new JobNotFoundException(jobUpdate.Id);
+
             var entity = _mapper.Map<Job>(jobUpdate);
 
-            _repository.Update(entity);
-            await _repository.SaveChangesAsync();
+            _jobRepository.Update(entity);
+            await _jobRepository.SaveChangesAsync();
         }
     }
 }
