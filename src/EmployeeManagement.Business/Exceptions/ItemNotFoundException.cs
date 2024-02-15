b5b602faa49c0c2886de5491a801bc79e8442964
Farzan Mosayyebi <farzanmosayyebi@gmail.com>
2024-02-15 17:15:07 +0330

    Implemented a Generic item not found exception

---
+++ src/EmployeeManagement.Business/Exceptions/ItemNotFoundException.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Serialization;
+
+namespace EmployeeManagement.Business.Exceptions
+{
+    [Serializable]
+    public class ItemNotFoundException : Exception
+    {
+        public Type ItemType { get; }
+        public int ItemId { get; }
+
+        public ItemNotFoundException()
+        {
+        }
+
+        public ItemNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public ItemNotFoundException(Type type, int id)
+        {
+            ItemType = type;
+            ItemId = id;
+        }
+
+        public ItemNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ItemNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
